b3b4db1635b9fb27d75e89ebfe47b86262bc6a10
Lukas Woodtli <woodtli.lukas@gmail.com>
2014-12-11 20:46:13 +0100

    Improve the code generator
    Now I't possible to generate basic classes. Some Tests need to be impelmented.

---
+++ PyCppGen/Program.py
@@ -0,0 +1,21 @@
+__author__ = 'Boot'
+import os
+
+
+class Program:
+    def __init__(self, sourceDirectory):
+        self.classes = []
+        self.sourceDir = sourceDirectory
+    def add_class(self, klass):
+        self.classes.append(klass)
+
+    def gen_source_files(self):
+        for klass in self.classes:
+            baseName = os.path.join(self.sourceDir, klass.name)
+            with open(baseName + ".hpp", 'w') as hppFile:
+                hppFile.write(klass.generate_header_code())
+            with open(baseName + ".cpp", 'w') as cppFile:
+                cppFile.write(klass.generate_implementation_code())
+
+
+
--- TestPyCppGen.py
+++ TestPyCppGen.py
@@ -1,6 +1,19 @@
 __author__ = 'Boot'
 
-import PyCppGen
+import os
+import PyCppGen.PyCppGen as PyCpp
+import PyCppGen.Program as p
 
-MyClass = PyCppGen.CppClass("MyClass")
+output_path = os.path.split(__file__)[0]
 
+
+program = p.Program(output_path)
+
+myClass = PyCpp.CppClass("MyClass")
+myMethod = PyCpp.CppMethod("myMethod", "void")
+myClass.add_method(myMethod)
+
+
+program.add_class(myClass)
+
+program.gen_source_files()
